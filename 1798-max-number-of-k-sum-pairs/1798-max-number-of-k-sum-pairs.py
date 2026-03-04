9b7eef3a1fefad5612b965b77f19fb860cffd72a
PidgerPodger <90056671+Uniciti@users.noreply.github.com>
2026-03-04 16:52:06 +0700

    1798-MAX-NUMBER-OF-K-SUM-PAIRS Stats: Time: 467 ms (54.34%), Space: 31.5 MB (64.99%) - LeetHub

--- 1798-max-number-of-k-sum-pairs/1798-max-number-of-k-sum-pairs.py
+++ 1798-max-number-of-k-sum-pairs/1798-max-number-of-k-sum-pairs.py
@@ -1,20 +1,17 @@
-import math
-
 class Solution:
     def maxOperations(self, nums: List[int], k: int) -> int:
-        count = {}
         result = 0
-        for x in nums:
-            count[x] = count.get(x, 0) + 1
-        for x in count.keys():
-            y = k - x
-            if y == x:
-                result += math.floor(count.get(x) / 2)
-                count[x] = count.get(x, 0) % 2
-
+        left = 0
+        right = len(nums) - 1
+        nums = sorted(nums)
+        while left < right:
+            if nums[left] + nums[right] < k:
+                left += 1
+            elif nums[left] + nums[right] > k:
+                right -= 1
             else:
-                min_key = x if count.get(x,0) < count.get(y,0) else y
-                result += min(count.get(x), count.get(y, 0))
-                if min_key in count:
-                    count[min_key] = 0
+                left += 1
+                right -= 1
+                result += 1
+        
         return result
