100dd85d57f087d400cbfc1239ef5f080a2bd852
PidgerPodger <90056671+Uniciti@users.noreply.github.com>
2026-03-26 17:46:18 +0700

    1777-DETERMINE-IF-TWO-STRINGS-ARE-CLOSE Stats: Time: 151 ms (39.71%), Space: 20.9 MB (8.47%) - LeetHub

--- 1777-determine-if-two-strings-are-close/1777-determine-if-two-strings-are-close.py
+++ 1777-determine-if-two-strings-are-close/1777-determine-if-two-strings-are-close.py
@@ -7,4 +7,12 @@ class Solution:
         for i in word2:
             d2[i] = d2.get(i, 0) + 1
 
-        return (sorted(d1.values()) == sorted(d2.values())) and (d1.keys() == d2.keys())
+        f1 = {}
+        f2 = {}
+
+        for j in d1.values():
+            f1[j] = f1.get(j, 0) + 1
+        for j in d2.values():
+            f2[j] = f2.get(j, 0) + 1
+
+        return (f1 == f2) and (d1.keys() == d2.keys())
