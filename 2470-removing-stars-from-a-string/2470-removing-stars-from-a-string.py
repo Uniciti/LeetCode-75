4d90bbd9c8f1d24a84e1998e4d47af839d9a2752
PidgerPodger <90056671+Uniciti@users.noreply.github.com>
2026-04-11 17:37:49 +0700

    2470-REMOVING-STARS-FROM-A-STRING Stats: Time: 106 ms (34.37%), Space: 21.3 MB (5.19%) - LeetHub

--- 2470-removing-stars-from-a-string/2470-removing-stars-from-a-string.py
+++ 2470-removing-stars-from-a-string/2470-removing-stars-from-a-string.py
@@ -4,7 +4,7 @@ class Solution:
     def removeStars(self, s: str) -> str:
         dq = deque(maxlen=len(s))
         for i in s:
-            if i == "*":
+            if i == '*':
                 dq.pop()
             else:
                 dq.append(i)
